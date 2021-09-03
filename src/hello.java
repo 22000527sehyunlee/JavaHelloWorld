bd6ceeea66b152a3b1abcef4e42fca5df1bb105c
세현 이 <22000527@handong.edu>
2021-09-03 12:03:07 +0900

    first commit111

---
+++ src/hello.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class hello {
+	public static void main(String[] args) {
+		Scanner s = new Scanner (System.in);
+		
+		System.out.ptintln("Hello World!!!\n");
+		System.out.print("이름 입력 : ");
+		String name = s.next();
+		System.out.println("["+name+"]님 환영합니다~");
+	}
+
+}
