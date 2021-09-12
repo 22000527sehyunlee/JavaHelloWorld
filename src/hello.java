32bd1fc76ce34118a272776e4285a63ca4d97544
세현 이 <22000527@handong.edu>
2021-09-12 14:43:21 +0900

    idk java

--- src/hello.java
+++ src/hello.java
@@ -9,6 +9,7 @@ public class hello {
 		System.out.println("이름 입력 : ");
 		String name = s.next();
 		System.out.println("["+name+"]님 환영합니다~");
+		System.out.println("대체 이거 왜이러는 거죠....? 자바 싫어")
 		s.close();
 	}
 }
